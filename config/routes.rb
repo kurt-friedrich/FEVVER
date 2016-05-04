1d50035c7016cf5b927a3f3c8f618e9afcc15804
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-05-04 15:27:53 -0400

    nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  resources :songs
-  resources :bands
-  resources :users
+  resources :users, except: [:index]
+
+  resources :bands do
+    resources :songs do
+      resources :attachments, only: [:new, :create]
+      resources :comments, only: [:new, :create]
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
