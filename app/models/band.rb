01b10e09065d8ed4cf58e6572436c9b07cd31e92
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-05-04 21:10:35 -0400

    create band index and some styling

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,8 +1,8 @@
 class Band < ActiveRecord::Base
-  belongs_to :owner, :class_name => ‘User’
+  #belongs_to :owner, class_name: ‘User’
   has_many :comments, as: :commentable
   has_many :memberships
-  has_many :users, :through => :memberships
+  has_many :users, through: :memberships
   has_many :songs
 
   def set_owner
