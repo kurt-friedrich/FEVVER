b1d91c88fd45fe32122bc7e4270d782ff55bdccc
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-05-15 17:56:26 -0400

    move page header into partial

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,16 @@ module ApplicationHelper
   def user_is_owner?
     current_user == @band.owner
   end
+
+  def icon_add
+    '<i class="fi-plus"></i>'
+  end
+
+  def icon_back
+    '<i class="fi-arrow-left"></i>'
+  end
+
+  def icon_edit
+    '<i class="fi-page-edit"></i>'
+  end
 end
