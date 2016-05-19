bb43d35a50d7fde67d923ee6b0715b6d4a55b456
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-05-19 14:21:29 -0400

    add dependent destroy to song attachments

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   belongs_to :band
-  has_many :attachments
+  has_many :attachments, dependent: :destroy
   has_many :comments, as: :commentable
   accepts_nested_attributes_for :attachments
 end
