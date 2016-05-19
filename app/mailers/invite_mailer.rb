840b0c5efdd8b7db2f6d921208db264e7493bf83
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-05-19 13:51:47 -0400

    finish mailers

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -3,13 +3,13 @@ class InviteMailer < ApplicationMailer
 
   def new_user_invite(invite, url)
     @invite = invite
-    @url = "https://salty-tundra-17115.herokuapp.com/" + url
-    mail(to: @invite.email, subject: 'Come join your band at FEVVER')
+    @url = "https://salty-tundra-17115.herokuapp.com" + url
+    mail(to: @invite.email, subject: 'Come join your band on FEVVER')
   end
 
   def existing_user_invite(invite)
-    @user = invite.recipient
-    @url = "https://salty-tundra-17115.herokuapp.com/"
-    mail(to: @user.email, subject: 'you were added to a band')
+    @invite = invite
+    @url = "https://salty-tundra-17115.herokuapp.com/login"
+    mail(to: @invite.email, subject: 'You were added to a band on FEVVER')
   end
 end
